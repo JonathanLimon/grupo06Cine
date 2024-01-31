95991f52a7c41fb6e524473ced9ef37f39e4fa4f
in1dw3 <in1dw3@HZ317804.elorrieta.int>
2024-01-31 18:48:57 +0100

    dutils para meter xampp a java

---
+++ CineElorrietaReto3G6/src/DUtils/DBUtils.java
@@ -0,0 +1,12 @@
+package DUtils;
+
+public class DBUtils {
+
+	public static final String URL = "jdbc:mysql://localhost:3307/cinesElorrieta";
+
+	public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+	
+	public static final String USER = "root";
+	public static final String PASS = "";
+	
+}
