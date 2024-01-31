5e8a19833ffa511a56475e86c71da11948a9a603
in1dw3 <in1dw3@HZ317804.elorrieta.int>
2024-01-31 19:34:33 +0100

    cambios ventana principal

--- CineElorrietaReto3G6/src/DUtils/DBUtils.java
+++ CineElorrietaReto3G6/src/DUtils/DBUtils.java
@@ -2,7 +2,7 @@ package DUtils;
 
 public class DBUtils {
 
-	public static final String URL = "jdbc:mysql://localhost:3307/cinesElorrieta";
+	public static final String URL = "jdbc:mysql://localhost:3307/cineselorrieta";
 
 	public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
 	
--- CineElorrietaReto3G6/src/vista/VentanaPrincipalIbai.java
+++ CineElorrietaReto3G6/src/vista/VentanaPrincipalIbai.java
@@ -44,12 +44,6 @@ public class VentanaPrincipalIbai {
 
 	private JFrame frame;
 
-	private GestorCines gestorCines = new GestorCines();
-	private GestorPeliculas gestorPeliculas = new GestorPeliculas();
-	private GestorProyecciones gestorProyecciones = new GestorProyecciones();
-	private GestorSalas gestorSalas = new GestorSalas();
-	private GestorEntradas gestorEntradas = new GestorEntradas();
-
 	public String tituloSeleccionado = null;
 	public Date fechaSeleccionada = null;
 	public Time horaSeleccionada = null;
@@ -58,11 +52,9 @@ public class VentanaPrincipalIbai {
 
 	public int numSalaSeleccionada = 0;
 	public int codCine = 0;
-	private int codProyeccion = 0;
 
 	public Float precioTotal = null;
 
-	private DefaultListModel<String> listModel = new DefaultListModel<String>();
 	DefaultTableModel model = new DefaultTableModel();
 	private JTextField txtNombre = null;
 	private JTextField txtApellido = null;
@@ -103,145 +95,169 @@ public class VentanaPrincipalIbai {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 
-		JPanel bPanelBienvenida = new JPanel();
-		bPanelBienvenida.setLayout(null);
-		bPanelBienvenida.setBounds(-90, -153, 1028, 857);
-		frame.getContentPane().add(bPanelBienvenida);
+		JPanel PanelLogin = new JPanel();
+		PanelLogin.setBounds(0, 0, 564, 441);
+		frame.getContentPane().add(PanelLogin);
+		PanelLogin.setVisible(false);
+		PanelLogin.setLayout(null);
 
-		JPanel rPanelRegistro = new JPanel();
-		rPanelRegistro.setBounds(0, 0, 564, 441);
-		frame.getContentPane().add(rPanelRegistro);
-		rPanelRegistro.setLayout(null);
-		rPanelRegistro.setVisible(false);
+		JPanel PanelRegistro = new JPanel();
+		PanelRegistro.setBounds(0, 0, 564, 441);
+		frame.getContentPane().add(PanelRegistro);
+		PanelRegistro.setLayout(null);
+		PanelRegistro.setVisible(false);
 
-		JPanel lPanelLogin = new JPanel();
-		lPanelLogin.setBounds(0, 0, 564, 441);
-		frame.getContentPane().add(lPanelLogin);
-		lPanelLogin.setVisible(false);
-		lPanelLogin.setLayout(null);
-
-		JLabel lblNewLabel = new JLabel("Registro");
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		lblNewLabel.setBounds(145, 11, 135, 26);
-		rPanelRegistro.add(lblNewLabel);
-
-		JLabel lblNewLabelNombre = new JLabel("Nombre");
-		lblNewLabelNombre.setBounds(81, 208, 46, 14);
-		rPanelRegistro.add(lblNewLabelNombre);
-
-		JLabel lblNewLabelApellido = new JLabel("Apellido");
-		lblNewLabelApellido.setBounds(81, 233, 46, 14);
-		rPanelRegistro.add(lblNewLabelApellido);
-
-		JLabel lblNewLabelDni = new JLabel("DNI");
-		lblNewLabelDni.setBounds(81, 261, 46, 14);
-		rPanelRegistro.add(lblNewLabelDni);
-
-		JLabel lblNewLabelSexo = new JLabel("Sexo");
-		lblNewLabelSexo.setBounds(81, 286, 46, 14);
-		rPanelRegistro.add(lblNewLabelSexo);
-
-		JLabel lblNewLabelUsuario = new JLabel("Usuario");
-		lblNewLabelUsuario.setBounds(293, 236, 46, 14);
-		rPanelRegistro.add(lblNewLabelUsuario);
-
-		JLabel lblNewLabelContraseña = new JLabel("Contraseña");
-		lblNewLabelContraseña.setBounds(283, 279, 56, 14);
-		rPanelRegistro.add(lblNewLabelContraseña);
+		JPanel PanelBienvenida = new JPanel();
+		PanelBienvenida.setLayout(null);
+		PanelBienvenida.setBounds(0, 0, 564, 441);
+		frame.getContentPane().add(PanelBienvenida);
 
 		JLabel lblLogin = new JLabel("LOGIN");
 		lblLogin.setBounds(249, 100, 66, 30);
 		lblLogin.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		lPanelLogin.add(lblLogin);
+		PanelLogin.add(lblLogin);
 
-		JLabel lblNewLabel_1 = new JLabel("Usuario");
-		lblNewLabel_1.setBounds(238, 172, 46, 14);
-		lPanelLogin.add(lblNewLabel_1);
+		JLabel lblUsuarioLogin = new JLabel("Usuario");
+		lblUsuarioLogin.setBounds(238, 172, 46, 14);
+		PanelLogin.add(lblUsuarioLogin);
 
-		JLabel lblNewLabel_2 = new JLabel("Contraseña");
-		lblNewLabel_2.setBounds(238, 234, 72, 14);
-		lPanelLogin.add(lblNewLabel_2);
+		JLabel lblContraseñaLogin = new JLabel("Contraseña");
+		lblContraseñaLogin.setBounds(238, 234, 72, 14);
+		PanelLogin.add(lblContraseñaLogin);
 
-		JLabel lblNewLabel_3 = new JLabel("¿No tienes cuenta? Registrate gratis");
-		lblNewLabel_3.setBounds(193, 15, 184, 14);
-		lPanelLogin.add(lblNewLabel_3);
+		JLabel lblTexto = new JLabel("¿No tienes cuenta? Registrate gratis");
+		lblTexto.setBounds(161, 15, 256, 14);
+		PanelLogin.add(lblTexto);
+
+		JButton btnRegistrarse = new JButton("Registrarme");
+		btnRegistrarse.setBounds(427, 11, 117, 23);
+		PanelLogin.add(btnRegistrarse);
+
+		JButton btnAccederCuenta = new JButton("Acceder");
+		btnAccederCuenta.setBounds(221, 334, 89, 23);
+		PanelLogin.add(btnAccederCuenta);
+
+		JButton btnAtrasLogin = new JButton("Atrás");
+		btnAtrasLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				PanelRegistro.setVisible(false);
+				PanelLogin.setVisible(false);
+				PanelBienvenida.setVisible(true);
+			}
+		});
+		btnAtrasLogin.setBounds(10, 11, 105, 23);
+		PanelLogin.add(btnAtrasLogin);
+
+		txtUserLogin = new JTextField();
+		txtUserLogin.setBounds(221, 197, 86, 20);
+		PanelLogin.add(txtUserLogin);
+		txtUserLogin.setColumns(10);
+
+		txtUserPass = new JPasswordField();
+		txtUserPass.setBounds(221, 259, 86, 20);
+		PanelLogin.add(txtUserPass);
+
+		// Action Listeners
+		btnRegistrarse.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				PanelLogin.setVisible(false);
+				PanelRegistro.setVisible(true);
+			}
+		});
+
+		JLabel lblRegistro = new JLabel("Registro");
+		lblRegistro.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblRegistro.setBounds(236, 54, 89, 26);
+		PanelRegistro.add(lblRegistro);
+
+		JLabel lblNombre = new JLabel("Nombre");
+		lblNombre.setBounds(81, 208, 46, 14);
+		PanelRegistro.add(lblNombre);
+
+		JLabel lblApellido = new JLabel("Apellido");
+		lblApellido.setBounds(81, 233, 46, 14);
+		PanelRegistro.add(lblApellido);
+
+		JLabel lblDni = new JLabel("DNI");
+		lblDni.setBounds(81, 261, 46, 14);
+		PanelRegistro.add(lblDni);
+
+		JLabel lblSexo = new JLabel("Sexo");
+		lblSexo.setBounds(81, 286, 46, 14);
+		PanelRegistro.add(lblSexo);
+
+		JLabel lblUsuario = new JLabel("Usuario");
+		lblUsuario.setBounds(293, 236, 46, 14);
+		PanelRegistro.add(lblUsuario);
+
+		JLabel lblContraseña = new JLabel("Contraseña");
+		lblContraseña.setBounds(283, 279, 56, 14);
+		PanelRegistro.add(lblContraseña);
+
+		JButton btnAtrasRegistro = new JButton("Atrás");
+		btnAtrasRegistro.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				PanelLogin.setVisible(true);
+				PanelRegistro.setVisible(false);
+			}
+		});
+		btnAtrasRegistro.setBounds(181, 361, 89, 23);
+		PanelRegistro.add(btnAtrasRegistro);
+
+		JButton btnAceptarRegistro = new JButton("Aceptar");
+		btnAceptarRegistro.setBounds(349, 361, 89, 23);
+		PanelRegistro.add(btnAceptarRegistro);
+
+		txtNombre = new JTextField();
+		txtNombre.setBounds(125, 205, 86, 20);
+		PanelRegistro.add(txtNombre);
+		txtNombre.setColumns(10);
+
+		txtApellido = new JTextField();
+		txtApellido.setBounds(125, 233, 86, 20);
+		PanelRegistro.add(txtApellido);
+		txtApellido.setColumns(10);
+
+		txtDni = new JTextField();
+		txtDni.setBounds(125, 258, 86, 20);
+		PanelRegistro.add(txtDni);
+		txtDni.setColumns(10);
+
+		txtUsuario = new JTextField();
+		txtUsuario.setBounds(349, 235, 86, 20);
+		PanelRegistro.add(txtUsuario);
+		txtUsuario.setColumns(10);
+
+		txtContras = new JPasswordField();
+		txtContras.setBounds(349, 276, 90, 20);
+		PanelRegistro.add(txtContras);
+
+		JComboBox<String> comboBoxSexo = new JComboBox<String>();
+		comboBoxSexo.setModel(new DefaultComboBoxModel<String>(new String[] { "Seleccione", "Hombre", "Mujer" }));
+		comboBoxSexo.setBounds(125, 286, 86, 22);
+		PanelRegistro.add(comboBoxSexo);
 
 		// Buttons
 
 		JButton bBotonBienvenida = new JButton("BIENVENIDO");
+		bBotonBienvenida.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				PanelBienvenida.setVisible(false);
+				PanelLogin.setVisible(true);
+			}
+		});
 		bBotonBienvenida.setIcon(new ImageIcon("img/bienvenida.png"));
 		bBotonBienvenida.setForeground(Color.BLACK);
 		bBotonBienvenida.setFont(new Font("Yu Gothic", Font.PLAIN, 50));
 		bBotonBienvenida.setFocusable(false);
 		bBotonBienvenida.setBackground(Color.WHITE);
-		bBotonBienvenida.setBounds(92, 154, 564, 441);
-		bPanelBienvenida.add(bBotonBienvenida);
+		bBotonBienvenida.setBounds(0, 0, 564, 441);
+		PanelBienvenida.add(bBotonBienvenida);
 
-		JButton btnAtrasRegistro = new JButton("Atrás");
-		btnAtrasRegistro.setBounds(181, 361, 89, 23);
-		rPanelRegistro.add(btnAtrasRegistro);
-
-		JButton btnAceptarRegistro = new JButton("Aceptar");
-		btnAceptarRegistro.setBounds(349, 361, 89, 23);
-		rPanelRegistro.add(btnAceptarRegistro);
-
-		JButton btnRegistrarse = new JButton("Registrarme");
-		btnRegistrarse.setBounds(440, 11, 104, 23);
-		lPanelLogin.add(btnRegistrarse);
-
-		JButton btnAccederCuenta = new JButton("Acceder");
-		btnAccederCuenta.setBounds(221, 334, 89, 23);
-		lPanelLogin.add(btnAccederCuenta);
-
-		JButton btnAtrasLogin = new JButton("Atrás");
-		btnAtrasLogin.setBounds(10, 11, 89, 23);
-		lPanelLogin.add(btnAtrasLogin);
-
-		txtNombre = new JTextField();
-		txtNombre.setBounds(125, 205, 86, 20);
-		rPanelRegistro.add(txtNombre);
-		txtNombre.setColumns(10);
-
-		txtApellido = new JTextField();
-		txtApellido.setBounds(125, 233, 86, 20);
-		rPanelRegistro.add(txtApellido);
-		txtApellido.setColumns(10);
-
-		txtDni = new JTextField();
-		txtDni.setBounds(125, 258, 86, 20);
-		rPanelRegistro.add(txtDni);
-		txtDni.setColumns(10);
-
-		txtUsuario = new JTextField();
-		txtUsuario.setBounds(349, 235, 86, 20);
-		rPanelRegistro.add(txtUsuario);
-		txtUsuario.setColumns(10);
-
-		txtContras = new JPasswordField();
-		txtContras.setBounds(349, 276, 90, 20);
-		rPanelRegistro.add(txtContras);
-
-		txtUserLogin = new JTextField();
-		txtUserLogin.setBounds(221, 197, 86, 20);
-		lPanelLogin.add(txtUserLogin);
-		txtUserLogin.setColumns(10);
-
-		txtUserPass = new JPasswordField();
-		txtUserPass.setBounds(221, 259, 86, 20);
-		lPanelLogin.add(txtUserPass);
-
-		JComboBox<String> comboBoxSexo = new JComboBox<String>();
-		comboBoxSexo.setModel(new DefaultComboBoxModel<String>(new String[] { "Seleccione", "Hombre", "Mujer" }));
-		comboBoxSexo.setBounds(125, 286, 86, 22);
-		rPanelRegistro.add(comboBoxSexo);
-
-		// Action Listeners
-		btnRegistrarse.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				lPanelLogin.setVisible(false);
-				rPanelRegistro.setVisible(true);
-			}
-		});
+		JLabel lblFondoBienvenida = new JLabel("");
+		lblFondoBienvenida.setBounds(0, 0, 564, 441);
+		PanelBienvenida.add(lblFondoBienvenida);
 
 	}
 
