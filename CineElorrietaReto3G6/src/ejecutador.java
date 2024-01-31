0796e45602e664e5cb857eec86bc4421f6981695
in1dw3 <in1dw3@HZ317804.elorrieta.int>
2024-01-31 18:52:56 +0100

    ejecutador y ventana Ibai de manera año pasado

---
+++ CineElorrietaReto3G6/src/ejecutador.java
@@ -0,0 +1,12 @@
+import vista.ventanaPrincipalIbai;
+
+public class ejecutador {
+
+	public static void main(String[] args) {
+
+		ventanaPrincipalIbai ventanaPrincipalIbai = new ventanaPrincipalIbai();
+		ventanaPrincipalIbai.main(args);
+		
+	}
+
+}
